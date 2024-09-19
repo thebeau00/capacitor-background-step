d86622ec3e92dd3d4c67f86bf03261208c411e8e
sdnetworks <sd@sdmall.co.kr>
2024-09-19 17:19:31 +0900

    rebuild

--- android/src/main/java/com/naeiut/plugins/backgroundstep/Backgroundstep.java
+++ android/src/main/java/com/naeiut/plugins/backgroundstep/Backgroundstep.java
@@ -1,11 +1,60 @@
 package com.naeiut.plugins.backgroundstep;
 
-import android.util.Log;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+
+import androidx.core.content.ContextCompat;
+import androidx.work.ExistingPeriodicWorkPolicy;
+import androidx.work.PeriodicWorkRequest;
+import androidx.work.WorkManager;
+
+import java.util.concurrent.TimeUnit;
 
 public class Backgroundstep {
 
     public String echo(String value) {
       //   Log.i("Echo", value);
         return value;
+    }
+
+
+    public static void startService(Context context, Activity activity) {
+        if (!StepCountBackgroundService.isServiceRunning) {
+            Intent serviceIntent = new Intent(context, StepCountBackgroundService.class);
+            ContextCompat.startForegroundService(context, serviceIntent);
+        }
+    }
+
+    public static void stopService(Context context, Activity activity) {
+        if (StepCountBackgroundService.isServiceRunning) {
+            StepCountBackgroundService stepCountBackgroundService = new StepCountBackgroundService();
+            Intent serviceIntent = new Intent(context, StepCountBackgroundService.class);
+            StepCountBackgroundService.stopForegroundService(context, activity);
+        }
+    }
+
+    public static void startServiceViaWorker(Context context) {
+        String UNIQUE_WORK_NAME = "StartMyServiceViaWorker";
+        WorkManager workManager = WorkManager.getInstance(context);
+
+        // As per Documentation: The minimum repeat interval that can be defined is 15 minutes
+        // (same as the JobScheduler API), but in practice 15 doesn't work. Using 16 here
+        PeriodicWorkRequest request =
+                new PeriodicWorkRequest.Builder(
+                        StepWorker.class,
+                        16,
+                        TimeUnit.MINUTES)
+                        .build();
+
+        // to schedule a unique work, no matter how many times app is opened i.e. startServiceViaWorker gets called
+        // do check for AutoStart permission
+        workManager.enqueueUniquePeriodicWork(UNIQUE_WORK_NAME, ExistingPeriodicWorkPolicy.KEEP, request);
+    }
+
+    public static void stopServiceViaWorker(Context context) {
+        String UNIQUE_WORK_NAME = "StartMyServiceViaWorker";
+        WorkManager workManager = WorkManager.getInstance(context);
+        workManager.cancelUniqueWork(UNIQUE_WORK_NAME);
     }
 }
--- android/src/main/java/com/naeiut/plugins/backgroundstep/BackgroundstepPlugin.java
+++ android/src/main/java/com/naeiut/plugins/backgroundstep/BackgroundstepPlugin.java
@@ -44,44 +44,6 @@ public class BackgroundstepPlugin extends Plugin {
 
     private String TAG = "BackgroundstepPlugin";
 
-    public static void startService(Context context, Activity activity) {
-        if (!StepCountBackgroundService.isServiceRunning) {
-            Intent serviceIntent = new Intent(context, StepCountBackgroundService.class);
-            ContextCompat.startForegroundService(context, serviceIntent);
-        }
-    }
-
-    public static void stopService(Context context, Activity activity) {
-        if (StepCountBackgroundService.isServiceRunning) {
-            StepCountBackgroundService stepCountBackgroundService = new StepCountBackgroundService();
-            Intent serviceIntent = new Intent(context, StepCountBackgroundService.class);
-            StepCountBackgroundService.stopForegroundService(context, activity);
-        }
-    }
-
-    public static void startServiceViaWorker(Context context) {
-        String UNIQUE_WORK_NAME = "StartMyServiceViaWorker";
-        WorkManager workManager = WorkManager.getInstance(context);
-
-        // As per Documentation: The minimum repeat interval that can be defined is 15 minutes
-        // (same as the JobScheduler API), but in practice 15 doesn't work. Using 16 here
-        PeriodicWorkRequest request =
-                new PeriodicWorkRequest.Builder(
-                        StepWorker.class,
-                        16,
-                        TimeUnit.MINUTES)
-                        .build();
-
-        // to schedule a unique work, no matter how many times app is opened i.e. startServiceViaWorker gets called
-        // do check for AutoStart permission
-        workManager.enqueueUniquePeriodicWork(UNIQUE_WORK_NAME, ExistingPeriodicWorkPolicy.KEEP, request);
-    }
-
-    public static void stopServiceViaWorker(Context context) {
-        String UNIQUE_WORK_NAME = "StartMyServiceViaWorker";
-        WorkManager workManager = WorkManager.getInstance(context);
-        workManager.cancelUniqueWork(UNIQUE_WORK_NAME);
-    }
 
     @PluginMethod
     public void echo(PluginCall call) {
@@ -96,8 +58,8 @@ public class BackgroundstepPlugin extends Plugin {
     @PluginMethod
     public void serviceStart(PluginCall call) {
 
-        startService(this.getActivity().getApplicationContext(), this.getActivity());
-        startServiceViaWorker(this.getActivity().getApplicationContext());
+        implementation.startService(this.getActivity().getApplicationContext(), this.getActivity());
+        implementation.startServiceViaWorker(this.getActivity().getApplicationContext());
 
         JSObject response = new JSObject();
         response.put("result", true);
@@ -109,8 +71,8 @@ public class BackgroundstepPlugin extends Plugin {
     @PluginMethod
     public void serviceStop(PluginCall call) {
 
-        stopService(this.getActivity().getApplicationContext(), this.getActivity());
-        stopServiceViaWorker(this.getActivity().getApplicationContext());
+        implementation.stopService(this.getActivity().getApplicationContext(), this.getActivity());
+        implementation.stopServiceViaWorker(this.getActivity().getApplicationContext());
 
         JSObject response = new JSObject();
         response.put("result", true);
